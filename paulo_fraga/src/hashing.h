05ffc4e9c3ae70a32c4bfd245a7ffc0981e20ead
paulohmf <paulohmacielf@gmail.com>
2019-11-20 14:50:07 -0300

    comentarios em hashing.h

--- paulo_fraga/src/hashing.h
+++ paulo_fraga/src/hashing.h
@@ -6,8 +6,8 @@
 
 
 struct myHashing{
-  int range = 25;
-  int chave = 97;
+  int range = 25; //Hashing de 0('a') até 25('z')
+  int chave = 97; //A posição é encontrada pelo resto da divisão por 'a' ou 97
   listaEncadeada *hashing[26];
 };
 
